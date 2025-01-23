9be1c9a10b0f75c2699a9796cc4719a98065e26d
Sujan Shrestha <sujans@gmeremit.com>
2025-01-23 20:21:37 +0900

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+import { createBrowserRouter, RouterProvider,BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/movie-details/:id" element={<MovieDetails />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all for unmatched routes */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
